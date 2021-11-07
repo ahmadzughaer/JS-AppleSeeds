48d56ed496ad697d18ff907d618f3c06cb6651bf
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-07 12:28:07 +0200

    assignment 2.1

---
+++ 2.1-functions.js
@@ -0,0 +1,8 @@
+function describeCountry(country, population, capitalCity) {
+
+    return country+' '+'has '+population+ ' people and its capital city is '+ capitalCity;
+}
+console.log(describeCountry('Italy', 60000000, 'Rome'));
+console.log(describeCountry('China', '1.4 Billion', 'Beijing'));
+console.log(describeCountry('Mexico', '129 million', 'Mexico city'));
+
