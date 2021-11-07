5b9f92adba746d1250401282cbc543aed5c3e02e
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-07 13:51:44 +0200

    assignment 2.3

---
+++ 2.3-circle area.js
@@ -0,0 +1,8 @@
+function circleArea(radius) {
+    let area = Math.PI*(radius*radius);
+    return area ;
+} 
+console.log(circleArea(16));
+let num = 16;
+console.log(circleArea(num).toFixed(2)); // it return string 
+
