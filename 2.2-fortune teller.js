70280eedaf0b228f95a312820ffb1423ce030bf3
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-07 12:51:45 +0200

    assignment 2.2

---
+++ 2.2-fortune teller.js
@@ -0,0 +1,7 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren ) {
+
+    return 'You will be '+jobTitle+' in '+location+' and married to '+partnerName+' with '+numberOfChildren+ ' children';
+}
+console.log(tellFortune('Developer', 'Jerusalem', 'Rinad', 3));
+
+
