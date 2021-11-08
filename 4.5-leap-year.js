d9eea1bd2c0599a1bdf1624ed8751b472d92782c
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-08 18:13:31 +0200

    assignment 4.5

---
+++ 4.5-leap-year.js
@@ -0,0 +1,11 @@
+const leapYear = (year) => {
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    }
+    else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+leapYear(2004);
+leapYear(2001);
