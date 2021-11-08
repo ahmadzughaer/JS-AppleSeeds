2f76ea0499d65d8cae6564cfb6c0f318be6dd7b4
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-08 13:55:50 +0200

    assignment 4.2

---
+++ 4.2-grade_assigner.js
@@ -0,0 +1,22 @@
+const gradeAssinger = (grade) => {
+if (grade >= 0 && grade <= 64 ) {
+    console.log('F')
+}
+else if (grade >= 65 && grade <= 69) {
+    console.log('D')
+}
+else if (grade >= 70 && grade <= 79) {
+    console.log('C')
+}
+else if (grade >= 80 && grade <= 89) {
+    console.log('B')
+}
+else if (grade >= 90 && grade <= 100) {
+    console.log('A')
+}
+else {
+    console.log('Error')
+
+}
+}
+gradeAssinger(99);
