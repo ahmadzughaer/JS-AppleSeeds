d9fba42da032c562b1e7997c78af40dc1aa39483
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-09 10:26:24 +0200

    assignment 4.1

---
+++ 4.1-number-of-siblings.js
@@ -0,0 +1,11 @@
+let numSiblings = prompt('How many siblings do you have?');
+if(numSiblings == 1) {
+    console.log(numSiblings + 'sibling!');
+}
+else if (numSiblings > 1) {
+    console.log(numSiblings + ' is greater than 1');
+}
+
+else {
+    console.log('No siblings');
+}
