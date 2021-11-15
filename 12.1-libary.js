1452c7f34a303b9df5bda850578d973f48fff639
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-15 20:24:08 +0200

    assignment 12.1

---
+++ 12.1-libary.js
@@ -0,0 +1,24 @@
+var library = [
+    {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+    },
+    {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+    },
+    {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+    }
+    ];
+
+const readAble =(obj) => {
+let book = obj.filter(el =>  el.readingStatus === true)
+return book;
+}
+
+console.log(readAble(library));
