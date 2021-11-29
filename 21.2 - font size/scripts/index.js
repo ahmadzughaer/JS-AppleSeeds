da700c5971c5978753831544c54fab19465a8082
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-29 13:21:48 +0200

    assignment 21.2

---
+++ 21.2 - font size/scripts/index.js
@@ -0,0 +1,30 @@
+
+let dec = document.querySelector('.dec');
+let inc = document.querySelector('.inc');
+let text = document.querySelector('.text');
+let num = 30;
+
+
+
+
+dec.addEventListener('click', () => {
+    if (num > 6) {
+        num--;
+        let font = text.style.fontSize = `${num}px`;
+        console.log(font)
+
+        return font;
+    }
+})
+
+
+inc.addEventListener('click', () => {
+    if (num < 100) {
+        num++;
+        let font = text.style.fontSize = `${num}px`;
+        console.log(font)
+
+        return font;
+    }
+})
+
