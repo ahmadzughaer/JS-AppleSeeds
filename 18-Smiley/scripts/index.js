785b277e3f0259fb6f9d1ac4673a5c7d1874edf5
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-29 16:35:53 +0200

    21.5

--- 18-Smiley/scripts/index.js
+++ 18-Smiley/scripts/index.js
@@ -1,28 +1,28 @@
-let input = document.querySelector('.input');
+let input = document.querySelector('#input');
+let box = document.querySelector('.box');
+
+input.addEventListener('input', isFocused = () => {
+    return box.innerHTML = '';
+})
 
 document.querySelector('.submit').addEventListener('click', () => {
     let num = input.value;
     let regex = /^[0-9]+$/;
+
     if (num.match(regex)) {
+
         for (let i = 0; i < +num; i++) {
-            let box = document.querySelector('.box');
             const div = document.createElement('div');
             div.innerHTML = `<div style="margin-top: 10px" class="smile"> 😄 </div>`
             box.appendChild(div)[i];
         };
+
     } else {
         alert('enter a number');
     }
-    return ;
 })
 
-// document.querySelector('.submit').addEventListener('click', () => {
-//     window.location.reload()
-// }) 
 
 
-// input.onkeyup = () => {
-//     location.reload()
-// }
 
 
