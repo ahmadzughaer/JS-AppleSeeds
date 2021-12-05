723fc589fc65453e902af5a229739d790ef6f437
ahmadzughaer <ahmadzughaer@gmail.com>
2021-12-05 14:21:44 +0200

    assignment 24.1

---
+++ 24.1-isSquare.js
@@ -0,0 +1,14 @@
+function Square(a,b,c,d) {
+    this.a = a;
+    this.b = b;
+    this.c = c ;
+    this.d = d;
+}
+
+Square.prototype.isSquare =  () => {
+    return (  this.a === this.b && this.c === this.d )
+}
+
+let mySquare = new Square(2,2,1,1)
+
+console.log(mySquare.isSquare())
