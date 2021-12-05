5e67f3fdaa5d19930e167cd09443c3f66c0b6422
ahmadzughaer <ahmadzughaer@gmail.com>
2021-12-05 14:05:21 +0200

    assignment 233.2

---
+++ 233.2-superPowers.js
@@ -0,0 +1,12 @@
+const storm = { 
+    superPower: 'Flying',
+    printSuperPower() {
+        console.log("my superpower is " + this.superPower); 
+       }
+   } 
+ 
+
+storm.printSuperPower() 
+
+
+
