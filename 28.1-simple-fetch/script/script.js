8251d3514fca57625676b796446d4d12bf88f8dc
ahmadzughaer <ahmadzughaer@gmail.com>
2021-12-07 14:07:32 +0200

    assignment 28.1

---
+++ 28.1-simple-fetch/script/script.js
@@ -0,0 +1,28 @@
+const jokebtn = document.querySelector(`.getJoke`);
+const jokeHolder = document.querySelector(`.joke p`);
+const jokeTitle= document.querySelector(`.joke h3`);
+const backBtn = document.querySelector('.backBtn');
+backBtn.style.display='none'
+
+const jokeButtonText = jokebtn.querySelector('.btn-text');
+
+const baseURL = 'https://api.jokes.one/jod';
+
+const getJoke = () => {
+  jokeHolder.textContent = 'Loading 🥪🥪🥪🥪';
+  fetch(baseURL)
+    .then((response) => response.json())
+    .then((joke) => (jokeHolder.textContent = joke.contents.jokes[0].joke.text, jokeTitle.textContent = joke.contents.jokes[0].joke.title ))
+    .catch((e) => (jokeHolder.textContent = 'try again'));
+};
+
+
+jokebtn.addEventListener('click', () => {
+  getJoke()
+  jokebtn.style.display = 'none'
+  backBtn.style.display='block'
+});
+
+backBtn.addEventListener('click', () => {
+  window.location.reload()
+})
